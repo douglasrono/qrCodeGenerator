dc6abecf936c4e8ba57aed1b56900b72a51bcf96
Douglas Kiprono <69076570+douglasrono@users.noreply.github.com>
2022-08-19 09:39:24 +0300

    added qr web api

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
+
 var container = document.querySelector(".container");
 var qrCodeBtn = document.querySelector(".qrbtn");
 const qrCode = document.querySelector(".qrCode");
  
  qrCodeBtn.addEventListener('click', ()=>{
+    var qrApi = https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=qrValue;
     qrCode.classList.add(".active");
     console.log("hello");
  });
