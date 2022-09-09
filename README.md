3d478d5804d309c82c56ec9b1fedf211ca4de005
itsdaglas <douglasronno@gmail.com>
2022-09-09 10:10:33 +0300

    second commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# qrCodeGenerator
-<p>Generate a qr code with google api. Enter a text or a url on the input field.</p>
---
+++ index.js
@@ -0,0 +1,17 @@
+
+let container = document.querySelector(".container"),
+   qrCodeBtn = document.querySelector(".qrbtn"),
+   qrCode = document.querySelector(".qrCode"),
+   qrGenerator = document.querySelector(".qrGenerator");
+ var  qrImage = document.querySelector(".qr-image img");
+ 
+qrCodeBtn.addEventListener('click', () => {
+   qrGenerator.innerText = "Generating..";
+   qrImage.addEventListener('load', () => {
+      qrImage.classList.add(active);
+      qrImage.src = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=qrValue`;
+   
+    console.log("hello");
+   })
+    
+ });
