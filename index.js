3c919a6e3f062ae92c5b9f40c1383dec9e4fce07
itsdaglas <douglasronno@gmail.com>
2022-09-26 07:19:25 +0300

    latest changes

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
 
-let container = document.querySelector(".container"),
-   qrCodeBtn = document.querySelector(".qrbtn"),
-   qrCode = document.querySelector(".qrCode"),
-   qrGenerator = document.querySelector(".qrGenerator");
- var  qrImage = document.querySelector(".qr-image img");
+let qrBtn = document.querySelector("#qrBtn"),
+    qrInputValue = document.querySelector("#qrInput").value,
+    qrImage = document.querySelector(".qr-image img");
  
-qrCodeBtn.addEventListener('click', () => {
-   qrGenerator.innerText = "Generating..";
-   qrImage.addEventListener('load', () => {
-      qrImage.classList.add(active);
-      qrImage.src = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=qrValue`;
+qrBtn.addEventListener('click', () => {
+   if (qrInputValue ='') {
+      return
+   }
+   else {
+        qrBtn.innerText = "Generating..";
+       qrImage.addEventListener('load', () => {
+      qrBtn.innerText = "Generate QR code";
+      qrImage.src = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${qrValue}`;
    
    })
+}
     
  });
